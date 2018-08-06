c1846d233c44687958ff38d7550a399d5634bb50
Colin Rundel <rundel@gmail.com>
2018-08-06 15:46:12 -0400

    Remove unneeded code

--- inst/include/util.hpp
+++ inst/include/util.hpp
@@ -5,26 +5,6 @@
 
 // [[Rcpp::plugins(cpp11)]]
 
-class graph;
 
-std::vector<std::vector<int> > to_0_indexing(std::vector<std::vector<int> > adj) {
-  for(size_t i=0; i<adj.size(); i++) {
-    for(size_t j=0; j<adj[i].size(); j++) {
-      adj[i][j]--;
-    }
-  }  
-  
-  return adj;
-}
-
-std::vector<std::vector<int> > to_1_indexing(std::vector<std::vector<int> > adj) {
-  for(size_t i=0; i<adj.size(); i++) {
-    for(size_t j=0; j<adj[i].size(); j++) {
-      adj[i][j]++;
-    }
-  } 
-  
-  return adj;
-}
 
 #endif
